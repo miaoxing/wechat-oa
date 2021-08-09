92d3cab9409a53a0ebd995e53cbc1669c98d4000
twinh <twinhuang@qq.com>
2021-08-09 23:27:59 +0800

    feat(wechat-oa): 检查微信登录，失效则返回授权地址给前台跳转

--- src/WechatOaPlugin.php
+++ src/WechatOaPlugin.php
@@ -3,6 +3,8 @@
 namespace Miaoxing\WechatOa;
 
 use Miaoxing\Plugin\BasePlugin;
+use Miaoxing\Plugin\Service\User;
+use Miaoxing\WechatOa\Service\WechatOaAccountModel;
 
 class WechatOaPlugin extends BasePlugin
 {
@@ -17,5 +19,25 @@ class WechatOaPlugin extends BasePlugin
             'url' => 'admin/wechat-oa/account',
             'name' => '公众号设置',
         ];
+    }
+
+    public function onCheckAuth()
+    {
+        if (0 !== strpos($this->req->getRouterPathInfo(), '/m-api/')) {
+            return;
+        }
+
+        $account = WechatOaAccountModel::findBy('type', WechatOaAccountModel::TYPE_SERVICE);
+        if (!$account || !$account->applicationId || !$account->applicationSecret) {
+            $this->logger->info('未设置服务号');
+            return;
+        }
+
+        $ret = User::checkLogin();
+        if ($ret->isErr()) {
+            // 跨域后台默认获取不到 Referrer，返回 %next% 给前台替换
+            $ret['next'] = $account->getOauth2Url('%next%', 'snsapi_base');
+            return $ret;
+        }
     }
 }
