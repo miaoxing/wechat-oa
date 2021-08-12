c083dd961e99b4ab6ae35bd0ced2ca6b95c0c2a8
twinh <twinhuang@qq.com>
2021-08-12 17:39:09 +0800

    feat(wechat-oa): `WechatOaAccountModel` 增加 `getApi` 和 `getOauth2Url` 方法
    - createApiService 改为 getApi，并缓存起来

--- src/Service/WechatOaAccountModel.php
+++ src/Service/WechatOaAccountModel.php
@@ -5,11 +5,63 @@ namespace Miaoxing\WechatOa\Service;
 use Miaoxing\Plugin\BaseModel;
 use Miaoxing\Plugin\Model\HasAppIdTrait;
 use Miaoxing\Plugin\Model\ModelTrait;
+use Miaoxing\Wechat\Service\WechatApi;
 use Miaoxing\WechatOa\Metadata\WechatOaAccountTrait;
 
+/**
+ * @mixin \ReqMixin
+ */
 class WechatOaAccountModel extends BaseModel
 {
     use HasAppIdTrait;
     use ModelTrait;
     use WechatOaAccountTrait;
+
+    public const TYPE_SUBSCRIPTION = 1;
+
+    public const TYPE_SERVICE = 2;
+
+    /**
+     * @var WechatApi
+     */
+    protected $api;
+
+    /**
+     * 获取当前账号的微信 API 服务
+     *
+     * @return WechatApi
+     */
+    public function getApi(): WechatApi
+    {
+        if (!$this->api) {
+            $this->api = new WechatApi([
+                'wei' => $this->wei,
+                'appId' => $this->applicationId,
+                'appSecret' => $this->applicationSecret,
+            ]);
+        }
+        return $this->api;
+    }
+
+    /**
+     * 生成网页授权地址
+     *
+     * @param string|null $url
+     * @param string $scope
+     * @return string
+     */
+    public function getOauth2Url(?string $url, string $scope): string
+    {
+        if (!$url) {
+            $url = $this->req->getUrl();
+        }
+
+        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='
+            . $this->applicationId . '&redirect_uri=' . urlencode($url) . '&response_type=code&scope=' . $scope;
+
+        if ($this->isAuthed) {
+            $url .= '&component_appid=' . wei()->wechatComponentApi->getAppId();
+        }
+        return $url . '#wechat_redirect';
+    }
 }
