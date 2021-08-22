0578af6f90b4f41bfb52364cb19ed4e717e3c4f2
twinh <twinhuang@qq.com>
2021-08-22 22:04:04 +0800

    test(wechat-oa): 测试增加后端服务号登录接口

--- tests/pages/m-api/wechat-oa/login/IndexTest.php
+++ tests/pages/m-api/wechat-oa/login/IndexTest.php
@@ -4,6 +4,7 @@ namespace MiaoxingTest\WechatOa\Pages\MApi\WechatOa\Login;
 
 use Miaoxing\Plugin\Service\Tester;
 use Miaoxing\Plugin\Test\BaseTestCase;
+use Miaoxing\Wechat\Service\WechatApi;
 use Miaoxing\WechatOa\Service\WechatOaAccountModel;
 
 class IndexTest extends BaseTestCase
@@ -27,5 +28,47 @@ class IndexTest extends BaseTestCase
 
         $this->assertRetSuc($ret);
         $this->assertSame('https://open.weixin.qq.com/connect/oauth2/authorize?appid=x&redirect_uri=https%3A%2F%2Ftest.com&response_type=code&scope=snsapi_base#wechat_redirect', $ret['url']);
+    }
+
+    public function testPost()
+    {
+        $wechatApi = $this->getServiceMock(WechatApi::class, [
+            'getOAuth2AccessTokenByAuth',
+        ]);
+
+        $account = $this->getModelServiceMock(WechatOaAccountModel::class, [
+            'findBy',
+            'getApi',
+        ]);
+
+        $account->setOption('table', 'wechat_oa_accounts');
+
+        $account->expects($this->once())
+            ->method('getApi')
+            ->willReturn($wechatApi);
+
+        $wechatApi->expects($this->once())
+            ->method('getOAuth2AccessTokenByAuth')
+            ->with([
+                'code' => 'test-code',
+            ])
+            ->willReturn(suc([
+                'scope' => 'snsapi_base',
+                'openid' => 'test-openid',
+                'unionid' => 'test-unionid',
+            ]));
+
+        $account->fromArray([
+            'applicationId' => 'x',
+            'applicationSecret' => 'y',
+        ]);
+
+        $account->expects($this->once())
+            ->method('findBy')
+            ->willReturn($account);
+
+        $ret = Tester::request(['code' => 'test-code', 'url' => 'https://test.com'])->post('/m-api/wechat-oa/login');
+        $this->assertRetSuc($ret);
+        $this->assertArrayHasKey('token', $ret);
     }
 }
