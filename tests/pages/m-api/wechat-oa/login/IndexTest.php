4818e952b83fcae902ac5c8bb4baeacefbba8c1f
twinh <twinhuang@qq.com>
2021-08-24 14:09:33 +0800

    test(wechat-oa): 测试授权失败返回重试 URL

--- tests/pages/m-api/wechat-oa/login/IndexTest.php
+++ tests/pages/m-api/wechat-oa/login/IndexTest.php
@@ -71,4 +71,44 @@ class IndexTest extends BaseTestCase
         $this->assertRetSuc($ret);
         $this->assertArrayHasKey('token', $ret);
     }
+
+    public function testPostWechatFail()
+    {
+        $wechatApi = $this->getServiceMock(WechatApi::class, [
+            'getOAuth2AccessTokenByAuth',
+        ]);
+
+        $account = $this->getModelServiceMock(WechatOaAccountModel::class, [
+            'findBy',
+            'getApi',
+        ]);
+
+        $account->setOption('table', 'wechat_oa_accounts');
+
+        $account->expects($this->once())
+            ->method('getApi')
+            ->willReturn($wechatApi);
+
+        $wechatApi->expects($this->once())
+            ->method('getOAuth2AccessTokenByAuth')
+            ->with([
+                'code' => 'test-code',
+            ])
+            ->willReturn(err('error', 1));
+
+        $account->fromArray([
+            'applicationId' => 'x',
+            'applicationSecret' => 'y',
+        ]);
+
+        $account->expects($this->once())
+            ->method('findBy')
+            ->willReturn($account);
+
+        $ret = Tester::request(['code' => 'test-code', 'url' => 'https://test.com'])->post('/m-api/wechat-oa/login');
+        $this->assertRetErr($ret);
+
+        $this->assertSame('很抱歉，微信授权失败，请返回再试。(error)', $ret['message']);
+        $this->assertSame('https://open.weixin.qq.com/connect/oauth2/authorize?appid=x&redirect_uri=https%3A%2F%2Ftest.com%3Fretry%3D1&response_type=code&scope=snsapi_base#wechat_redirect', $ret['retryUrl']);
+    }
 }
