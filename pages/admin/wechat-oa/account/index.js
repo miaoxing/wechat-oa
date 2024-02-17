1abf59150c412d86a082d0d6934b72cd3cabc783
twinh <twinhuang@qq.com>
2024-02-17 21:38:04 +0800

    fix(wechat-oa): 解决 antd5 radio 变为居中后，label 多行文字不对齐的问题

--- pages/admin/wechat-oa/account/index.js
+++ pages/admin/wechat-oa/account/index.js
@@ -1,7 +1,8 @@
-import {Page, PageActions} from '@mxjs/a-page';
-import {Form, FormAction, FormItem} from '@mxjs/a-form';
-import {Radio, Space} from 'antd';
-import {Box} from '@mxjs/box';
+import { Page, PageActions } from '@mxjs/a-page';
+import { Form, FormAction, FormItem } from '@mxjs/a-form';
+import { Radio, Space, Typography } from 'antd';
+
+const {Text} = Typography;
 
 const Index = () => {
   return (
@@ -23,12 +24,13 @@ const Index = () => {
             <Space direction="vertical">
               <Radio value="snsapi_base">
                 静默授权（snsapi_base）
-                <Box gray400>不弹出授权页面，直接跳转，只能获取用户 openid</Box>
               </Radio>
+              <Text type="secondary">不弹出授权页面，直接跳转，只能获取用户 openid</Text>
               <Radio value="snsapi_userinfo">
                 弹出授权页面（snsapi_userinfo）
-                <Box gray400>弹出授权页面，可通过 openid 拿到昵称、性别、所在地。并且， 即使在未关注的情况下，只要用户授权，也能获取其信息 </Box>
               </Radio>
+              <Text type="secondary">弹出授权页面，可通过 openid 拿到昵称、性别、所在地。并且，
+                即使在未关注的情况下，只要用户授权，也能获取其信息</Text>
             </Space>
           </Radio.Group>
         </FormItem>
